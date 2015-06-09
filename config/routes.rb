00948dca75434c38cc066bb4f9dddbfbc9fd043a
Bezbo <without.a.beard@gmail.com>
2015-06-09 15:09:02 +0300

    complete general task

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Flashcards::Application.routes.draw do
+  root to: "cards#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
