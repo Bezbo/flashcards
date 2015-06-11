0d822d2aef28c90e0c00e045174eb94031e879e0
Bezbo <without.a.beard@gmail.com>
2015-06-11 10:57:49 +0300

    CRUD and validations for cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,12 @@
 class Card < ActiveRecord::Base
+  validates :original_text,
+            :translated_text,
+            :review_date, presence: true
+  validate :check_original_and_translated_texts
+
+  def check_original_and_translated_texts
+    if original_text.downcase.strip == translated_text.downcase.strip
+      errors.add(:translated_text, "не может быть равен оригиналу")
+    end
+  end
 end
