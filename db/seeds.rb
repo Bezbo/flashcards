b52e90d65c40b4974d87137d5c19d44c73d0e041
Bezbo <without.a.beard@gmail.com>
2015-06-12 21:27:32 +0300

    add nokogiri, set up seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+require 'nokogiri'
+
+Card.delete_all
+
+url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
+document = Nokogiri::HTML(open(url))
+index = document.css("tr+ tr")
+
+index.take(15).each do |word|
+  original_text = word.at_css("td:nth-child(2)").text
+  translated_text = word.at_css("td:nth-child(3)").text
+  Card.create(original_text: original_text,
+              translated_text: translated_text,
+              review_date: Time.now)
+end
