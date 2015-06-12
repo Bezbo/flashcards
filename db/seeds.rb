78b171ce11e6fc2b3cf5adb63f333651a7405368
Bezbo <without.a.beard@gmail.com>
2015-06-13 02:11:02 +0300

    fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@ Card.delete_all
 
 url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 document = Nokogiri::HTML(open(url))
-index = document.css("tr+ tr")
+index = document.css("tr + tr")
 
 index.take(15).each do |word|
-  original_text = word.at_css("td:nth-child(2)").text
+  original_text   = word.at_css("td:nth-child(2)").text
   translated_text = word.at_css("td:nth-child(3)").text
   Card.create(original_text: original_text,
               translated_text: translated_text,
